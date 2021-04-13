78239c435e1a6ca770cb843cc77654d4099f01d1
Dusan Stevic <stevicdule@gmail.com>
2021-04-13 16:56:04 +0200

    report grammar update

--- report/generator.py
+++ report/generator.py
@@ -10,6 +10,8 @@ import pdfkit
 import os
 # Set wkhtmltopdf Path variable
 os.environ["PATH"] += os.pathsep + 'C:/Program Files/wkhtmltopdf/bin'
+from textx import metamodel_from_file
+from textx.export import metamodel_export, model_export
 
 try:
     # Connect to an existing dsl database
@@ -57,25 +59,33 @@ srcgen_folder = join(this_folder, 'srcgen')
 if not exists(srcgen_folder):
     mkdir(srcgen_folder)
 
+grammar_folder = join(this_folder, 'grammar')
+models_folder = join(this_folder, 'models')
+# Get meta-model from language description
+report_metamodel = metamodel_from_file(join(grammar_folder, 'report.tx'), debug=False)
+# Instantiate model
+report_model = report_metamodel.model_from_file(join(models_folder, 'report.rprt'))
 # Initialize template engine.
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(templates_folder),trim_blocks=True,lstrip_blocks=True)
-
-# Load template
-template = jinja_env.get_template('TabularDetails.j2')
-template1 = jinja_env.get_template('GraphicalDetails.j2')
-#data = json.dumps( [1.0,2.0,3.0] )
-data = json.dumps(df["Close"].tolist())
-#labels=json.dumps( ["18-12-31", "19-01-01", "19-01-02"] )
-labels=json.dumps(df["Date"].tolist())
-topic = "Yahoo Finance Charts"
-
-
 # Generate report.html file
 with open(join(srcgen_folder, "report.html"), 'w') as f:
-    f.write(template.render(columns=columns,rows=rows))
-    f.write(template1.render(data=data, labels=labels, topic=topic))
-    
+    for details in report_model.report.details:
+        if(details.report_details_type == "tabular"):
+            # Load tabular template
+            template = jinja_env.get_template('TabularDetails.j2')
+            f.write(template.render(columns=columns,rows=rows))
+
+        elif(details.report_details_type == "graphical"):
+            # Load graphical template
+            template = jinja_env.get_template('GraphicalDetails.j2')
+            data = json.dumps(df[details.fields[0].value].tolist())
+            #labels=json.dumps( ["18-12-31", "19-01-01", "19-01-02"] )
+            labels=json.dumps(df["Date"].tolist())
+            topic = "Yahoo Finance Charts"
+            f.write(template.render(data=data, labels=labels, topic=topic))
+        else:
+            print("Minimum one report detail is required. Please add report details and try again.")
+
 # Generate report.pdf file from report.html file (wkhtmltopdf utility to convert HTML to PDF using Webkit)
 pdfkit.from_file(join(srcgen_folder, "report.html"), join(srcgen_folder, "report.pdf"))
 
-print(df)
